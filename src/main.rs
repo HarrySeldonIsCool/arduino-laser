e0652e307ba7c23413fad4d32b301768b45cfc51
david <david@kosatovi.cz>
2021-11-14 18:08:49 +0100

    presentable

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,38 @@
-use serialport::SerialPort;
-
-//wait until there's some data to read
-fn wait_for_answer(port: &mut Box<dyn SerialPort>) -> Result<(), std::io::Error>{
-    while port.bytes_to_read()? == 0{
-        std::thread::sleep(std::time::Duration::from_millis(50));
-    }
-    Ok(())
-}
-
-//send array of bytes, then wait for response and put it into buffer (as much as can fit)
-fn send_n_get(port: &mut Box<dyn SerialPort>, sent: &[u8], buffer: &mut [u8]) -> Result<(), std::io::Error>{
-    port.clear(serialport::ClearBuffer::Input)?;
-    port.write(sent)?;
-    wait_for_answer(port)?;
-    port.read(buffer)?;
-    Ok(())
-}
+mod arduino;
+use self::arduino::*;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button};
 
 fn main() -> Result<(), std::io::Error>{
-    //initialise new serial port and establish connection
-    let mut port = serialport::new("/dev/ttyACM0", 115200).open()?;
-    //initialise buffer
-    let mut buffy = [0; 4];
+    let app = Application::builder()
+        .application_id("org.gtk-rs.example")
+        .build();
 
-    //wait for some signal, that means connection is ready
-    wait_for_answer(&mut port)?;
-    //clear input buffer so that it won't get messy
-    port.clear(serialport::ClearBuffer::Input)?;
+    app.connect_activate(build_ui);
+    // Run the application
+    app.run();
+    
+    //let r = get_data()?;
+    Ok(())
+}
 
-    //repeatedly send command 'w' and print the response as integer
-    loop{
-        send_n_get(&mut port, &[b'w'], &mut buffy)?;
-        let x = i32::from_le_bytes(buffy);
-        eprint!("\rValue is: {}", x);
-    }
+fn build_ui(app: &Application){
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .build();
+        
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
 
-    //Ok(())
+    button.connect_clicked(move |button| {button.set_label("Hello world!")});
+
+    window.set_child(Some(&button));
+    // Present window to the user
+    window.present();
 }
