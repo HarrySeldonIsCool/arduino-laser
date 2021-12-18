dfda1cd994de0d46f477099bb236504893e6dcc7
david <david@kosatovi.cz>
2021-12-18 21:29:18 +0100

    uz to konecne funguje

--- src/macro_paralel_test.cpp
+++ src/macro_paralel_test.cpp
@@ -10,7 +10,8 @@ typedef short pt;
 #define HNUS_ZNOVA() HNUS
 
 #define HNUS(stop, ab, ugly, ar, ...)                           \
-    if (*(ab) != 1) {                                                \
+    pt abb ## ugly = *(ab);                                     \
+    if (*(ab) != 1) {                                           \
         BEGIN_PT(*(ab))                                         \
         ar;                                                     \
         END_PT(*(ab), ugly)                                     \
@@ -29,18 +30,18 @@ typedef short pt;
 
 #define BEGIN_PT(p) switch(p){case 0: case 1:
 
-#define LET_OTHERS(x) LET_OTHERS1(*(ab), x)
+#define LET_OTHERS(x) LET_OTHERS1(abb ## x, x)
 
 #define LET_OTHERS1(p, ugly) p = __LINE__; goto left_to_end ## ugly; case __LINE__: 
 
 #define END_PT(p, ugly) }p = 1; left_to_end ## ugly: 
 
 int main(){
-    static int d = 4;
-    static int b = 3;
-    static int c = 2;
+    static int a = 2;
+    static int b = 1;
+    static int c = 3;
     PARALEL(3, {
-        while(d--){
+        while(a--){
             cout << 1 << endl;
             LET_OTHERS(a);
         }
@@ -56,5 +57,3 @@ int main(){
         }
     });
 }
-
-/*{ pt ab[3] = { }; bool stop = true; while(stop){ stop = false; if (*(ab) != 1) { switch(*(ab)){case 0: case 1: { while(d--){ cout << 1 << endl; *(ab) = 45; goto left_to_enda; case 45:; } }; }*(ab) = 1; left_to_enda: stop = true; } if (*(ab+1) != 1) { switch(*(ab+1)){case 0: case 1: { while(b--){ cout << 2 << endl; *(ab) = 50; goto left_to_endaa; case 50:; } }; }*(ab+1) = 1; left_to_endaa: stop = true; } if (*(ab+1 +1) != 1) { switch(*(ab+1 +1)){case 0: case 1: { while(c--){ cout << 3 << endl; *(ab) = 55; goto left_to_endaaa; case 55:; } }; }*(ab+1 +1) = 1; left_to_endaaa: stop = true; } } }*/
